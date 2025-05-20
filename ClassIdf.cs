97206729144f99ea350182b605816150f8de4b3c
israelr9897 <israelr9897@gmail.com>
2025-05-20 17:36:26 +0300

    class Idf

---
+++ ClassIdf.cs
@@ -0,0 +1,12 @@
+namespace IDF_Operation
+{
+    internal class Idf
+    {
+        public string DateOfEstablishment = "01/05/1948";
+        public string CurrentCommander = "Eyal Zamir";
+        public List<object> StrikeOptions;
+
+        
+
+    }
+}
