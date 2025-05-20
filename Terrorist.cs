2c978f4817ac3873666e1191d499ce76d2b99914
coby98765 <coby98765@gmail.com>
2025-05-20 17:58:03 +0300

    class Terrorist

---
+++ Terrorist.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IDF_Operation.terrorist
+{
+    internal class Terrorist
+    {
+        public string Name { get;}
+        public int Rank { get; }
+        public string Status { get; set; }
+        public List<string> Weapons { get;}
+
+
+        public Terrorist(string name, int rank, string status, List<string> weapons) 
+        {
+            Name = name; 
+            Rank = rank; 
+            Status = status; 
+            Weapons = weapons;
+        }
+    }
+}
