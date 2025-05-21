7d7c440abb660cc9fb9ca680a68f49fca6da4724
coby98765 <coby98765@gmail.com>
2025-05-21 11:28:32 +0300

    Initializer plan ,and orgenize to folders

---
+++ Class1.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IDF_Operation.init
+{
+    internal class Initializer
+    {
+        public Initializer() 
+        {
+            //Initialize AttackOps
+
+            //Initialize IDF officers
+
+            //Initialize Hamas Terrorists
+
+            //Initialize Intel Reports
+
+        }
+    }
+}
