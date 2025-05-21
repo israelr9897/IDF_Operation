8c502da2c9cbc9c67e70b59cc698a17af1519223
israelr9897 <israelr9897@gmail.com>
2025-05-21 10:02:36 +0300

    finsh classes subSO

--- ClassStrikeOptions.cs
+++ ClassStrikeOptions.cs
@@ -2,17 +2,17 @@ namespace IDF_Operation
 {
     internal class StrikeOptions
     {
-        public string Name;
-        public Dictionary<string, int> AmmunitionAmount;
-        public int FuelSupply;
-        public string TypeTarget;
+        public string Name { get; }
+        public Dictionary<string, int> AmmunitionAmount { get; }
+        public int FuelSupply { get; }
+        public string TargetType { get; }
 
-        public StrikeOptions(string name, Dictionary<string, int> ammunitionAmount, int fuelSupply, string typeTarget)
+        public StrikeOptions(string name, Dictionary<string, int> ammunitionAmount, int fuelSupply, string targetType)
         {
             this.Name = name;
             this.AmmunitionAmount = ammunitionAmount;
             this.FuelSupply = fuelSupply;
-            this.TypeTarget = typeTarget;
+            this.TargetType = targetType;
         }
 
     }
--- ClassesSubSO.cs
+++ ClassesSubSO.cs
@@ -2,14 +2,18 @@ namespace IDF_Operation
 {
     internal class F16 : StrikeOptions
     {
-        public F16(string name, Dictionary<string, int> ammunitionAmount, int fuelSupply, string typeTarget) : base("F16", new Dictionary<string, int> { {"1", 4}, {"0.5", 4}}, 100, "Buildings"){}
+        public string PilotName { get; }
+        public F16(string name, Dictionary<string, int> ammunitionAmount, int fuelSupply, string typeTarget, string pilotName) : base("F16", new Dictionary<string, int> { { "1", 4 }, { "0.5", 4 } }, fuelSupply, "Buildings")
+        {
+            this.PilotName = pilotName;
+        }
     }
     internal class Zik : StrikeOptions
     {
-        public Zik(string name, Dictionary<string, int> ammunitionAmount, int fuelSupply, string typeTarget) : base("Zik", new Dictionary<string, int> { {"1", 4}, {"0.5", 4}}, 100, "Buildings"){}
+        public Zik(string name, Dictionary<string, int> ammunitionAmount, int fuelSupply, string typeTarget) : base("Zik 460", new Dictionary<string, int> { {"personnel", 1}, {"armored vehicles", 2}}, fuelSupply, " people, vehicles"){}
     }
     internal class M109 : StrikeOptions
     {
-        public M109(string name, Dictionary<string, int> ammunitionAmount, int fuelSupply, string typeTarget) : base("M109", new Dictionary<string, int> { {"1", 4}, {"0.5", 4}}, 100, "Buildings"){}
+        public M109(string name, Dictionary<string, int> ammunitionAmount, int fuelSupply, string typeTarget) : base("M109", new Dictionary<string, int> { {"shell", 40}}, fuelSupply, "open areas"){}
     }
 }
