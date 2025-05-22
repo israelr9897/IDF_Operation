29e845d3bab1a8b807f0e49bbd2245346f665038
israelr9897 <israelr9897@gmail.com>
2025-05-22 14:40:58 +0300

    function SortedNumberReports

--- Program.cs
+++ Program.cs
@@ -6,7 +6,11 @@ namespace IDF_Operation
     {
         static void Main(string[] args)
         {
-        
+            InitTerrorists t = new InitTerrorists(5);
+            InitReports v = new InitReports(5);
+            // Aman.ReportPrint();
+            Aman.SortedNumberReports();
+            // Aman.InitDict();
         }
     }
 
