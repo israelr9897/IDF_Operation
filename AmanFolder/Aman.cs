c8cd34bf71e24f5c707e5fb6d6581be2526c80b1
coby98765 <coby98765@gmail.com>
2025-05-22 12:54:07 +0300

    sort a list of intel report by date and time

--- AmanFolder/Aman.cs
+++ AmanFolder/Aman.cs
@@ -4,5 +4,13 @@ namespace IDF_Operation.Models
     static internal class Aman
     {
         static public List<IntelReport> ReportList = new List<IntelReport>();
+
+        static public List<IntelReport> SortReportsByDate(List<IntelReport> UnSorted)
+        {
+            return UnSorted.OrderBy(report => report.TimeStamp).ToList();
+        }
     }
+
+    
+
 }
