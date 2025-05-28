bc665e6a4ef9fbd5fa58c6ea289fdc627d5a77ee
coby98765 <coby98765@gmail.com>
2025-05-28 17:00:20 +0300

    StrickOpsCase

--- System/StrikeOpsOverview.cs
+++ System/StrikeOpsOverview.cs
@@ -13,10 +13,25 @@ namespace IDF_Operation.Models
         public StrikeOptions StrickOpsCase(string location)
         {
             List<StrikeOptions> WeaponList = SearchWeapon.FilterWeaponByType(location);
+            int SelectedStrickOp = ChooseStrickOp(WeaponList);
+            return SearchWeapon.SearchWeaponById(SelectedStrickOp);
+        }
+
+        private static int ChooseStrickOp(List<StrikeOptions> WeaponList)
+        {
             Console.WriteLine("Select Strick Option By entering it's ID: ");
             WeaponsPrint.Print(WeaponList);
             int StrickOpID = Convert.ToInt32(Console.ReadLine());
-            return SearchWeapon.SearchWeaponById(StrickOpID);
+            return StrickOpID;
         }
+
+        public static string AmmoCase(StrikeOptions StrikeOp)
+        {
+            //send srtikeOp to func for user to choose the ammo type
+            string ammoType = "";
+            return ammoType;
+        }
+
+
     }
 }
